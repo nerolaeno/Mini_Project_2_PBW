f2c435ba67f32b5887ff54bbef3febcebde0a56c
Lawrend Pandiangan <nerolaeno@gmail.com>
2026-03-25 02:20:32 +0800

    first commit

---
+++ koneksi.php
@@ -0,0 +1,8 @@
+﻿<?php
+$conn = mysqli_connect("localhost", "root", "", "web_portofolio");
+
+if (!$conn) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conn, "utf8mb4");
